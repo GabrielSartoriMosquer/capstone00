6116e5249b935afc6f3bbffdbd35c2981aef200d
GabrielSartoriMosquer <gabriel.sartori.mosquer@gmail.com>
2026-08-06 23:52:17 -0300

    fix: exceptions

--- parser.py
+++ parser.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import sqlite3
 import re
 
+API_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
 def log_fields_separator(log: str) -> dict:
 
     regex = r'^(?P<ip>\S+)\s+(?P<ident>\S+)\s+(?P<user>\S+)\s+\[(?P<datetime>[^\]]+)\]\s+"(?P<request>[^"]+)"\s+(?P<status>\d+)\s+(?P<bytes>\d+|-)$'
@@ -14,23 +16,33 @@ def log_fields_separator(log: str) -> dict:
 
 def parse_ip(ip):
     parts = ip.split('.')
-    for part in parts:
-        try:
-            if 0<=int(part)<255:
-                continue
-            else:
-                raise Exception
-        except ValueError as e:
-            return f'ERRO: Invalid IP format.\n{e}'
-        except Exception as e:
-            return f'Invalid number (out of IP range)./n{e}'
-    return ip
+    if len(parts) == 4:
+        for part in parts:
+            try:
+                if 0<=int(part)<255: #aqui pode subir um ValueError
+                    continue
+                else:
+                    raise InvalidIP(f'IP: {part} is out of the range (0-255).') #aqui eu subo número inválido
+        return ip
+    else:
+        raise IPParts(f'IP: {ip} has not 4 parts.')
+                
 
-def parse_date(datetime):
-    pass
+def parse_date(date):
+    try:
+        date = date.strftime('%d/%b/%Y:%H:%M:%S %z')
+        return date
+    except ValueError as e:
+        return f'Error: {e}'
 
 def parse_request(request):
-    pass
+    parts = request.split()
+    if request[0] in API_METHODS:
+        if re.match(request[1], r'/^[a-zA-Z0-9]+$'):
+            if re.match(request[2], r'HTTP/\d+(?:\.\d+)?\s+[1-5]\d\d'):
+                return request
+    return 'Error: Invalid request.'
+
 
 def parse_status(status):
     try:
