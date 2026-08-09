dead952dd98a5d72235f6fa64e82ce1890e4155d
GabrielSartoriMosquer <gabriel.sartori.mosquer@gmail.com>
2026-08-09 18:24:48 -0300

    fix: parsers (incomplete)

--- a.py
+++ a.py
@@ -1,14 +1,30 @@
-def parse_status(status):
+class InvalidField(Exception):
+    pass
+from datetime import datetime
+import re
+
+def log_fields_separator(log: str) -> dict:
+
+    regex = r'^(?P<ip>\S+)\s+(?P<ident>\S+)\s+(?P<user>\S+)\s+\[(?P<datetime>[^\]]+)\]\s+"(?P<request>[^"]+)"\s+(?P<status>\S+)\s+(?P<bytes>\S+|-)$'
+
+    match = re.match(regex, log)
+
+    if match:
+        fields = match.groupdict()
+        return fields
+
+a = log_fields_separator('192.168.35.32 - - [06/Aug/2026:22:15:02 -0300] "POST /health HTTP/1.1" 62 2a42')
+
+
+def parse_bytes(bytes):
     try:
-        if len(str(status))==3 and 100<=int(status)<=599:
-            return status
+        bytes = int(bytes)
+        if bytes < 0:
+            raise InvalidField('BYTES: Number of bytes below zero.')
         else:
-            return 'ERROR: Invalid status code.'
+            return bytes
     except ValueError as e:
-        return f'ERROR: {e}'
-    except Exception as e:
-        return f'ERROR: {e}'
+        raise InvalidField(f'BYTES: {e}')
 
-print(parse_status(100))
-
-
+b = parse_bytes(a['bytes'])
+print(b)
--- parser.py
+++ parser.py
@@ -4,9 +4,12 @@ import re
 
 API_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
+class InvalidField(Exception):
+    pass
+            
 def log_fields_separator(log: str) -> dict:
 
-    regex = r'^(?P<ip>\S+)\s+(?P<ident>\S+)\s+(?P<user>\S+)\s+\[(?P<datetime>[^\]]+)\]\s+"(?P<request>[^"]+)"\s+(?P<status>\d+)\s+(?P<bytes>\d+|-)$'
+    regex = r'^(?P<ip>\S+)\s+(?P<ident>\S+)\s+(?P<user>\S+)\s+\[(?P<datetime>[^\]]+)\]\s+"(?P<request>[^"]+)"\s+(?P<status>\S+)\s+(?P<bytes>\S+|-)$'
 
     match = re.match(regex, log)
 
@@ -19,52 +22,48 @@ def parse_ip(ip):
     if len(parts) == 4:
         for part in parts:
             try:
-                if 0<=int(part)<255: #aqui pode subir um ValueError
-                    continue
-                else:
-                    raise InvalidIP(f'IP: {part} is out of the range (0-255).') #aqui eu subo número inválido
+                if not 0<=int(part)<=255: 
+                    raise InvalidField(f'IP: {part} is out of the range (0-255).')  
+            except ValueError as e:
+                raise InvalidField(f'IP: {e}')
         return ip
     else:
-        raise IPParts(f'IP: {ip} has not 4 parts.')
+        raise InvalidField(f'IP: {ip} has not 4 parts.')
                 
-
 def parse_date(date):
-    try:
-        date = date.strftime('%d/%b/%Y:%H:%M:%S %z')
-        return date
-    except ValueError as e:
-        return f'Error: {e}'
+    date = date.strptime('%d/%b/%Y:%H:%M:%S %z')
+    return date
 
 def parse_request(request):
     parts = request.split()
-    if request[0] in API_METHODS:
-        if re.match(request[1], r'/^[a-zA-Z0-9]+$'):
-            if re.match(request[2], r'HTTP/\d+(?:\.\d+)?\s+[1-5]\d\d'):
-                return request
-    return 'Error: Invalid request.'
-
+    if len(parts) == 3:
+        print(1)
+        if parts[0] in API_METHODS:
+            print(2)
+            if re.match(r'^/[a-zA-Z0-9]+$', parts[1]):
+                print(3)
+                if re.match(r'^HTTP/\d+(?:\.\d+)?$', parts[2]):
+                    return parts[0], parts[1], parts[2]
+    raise InvalidField('REQUEST: the request is wrong.')
 
 def parse_status(status):
     try:
         if len(str(status))==3 and 100<=int(status)<=599:
-            return status
+            return int(status)
         else:
-            return 'ERROR: Invalid status code.'
+            raise InvalidField('STATUS: number of digits or incorrect status number.')
     except ValueError as e:
-        return f'ERROR: {e}'
-    except Exception as e:
-        return f'ERROR: {e}'
+        raise InvalidField(f'STATUS: {e}')
 
 def parse_bytes(bytes):
     try:
-        int(bytes)
-        return bytes
+        bytes = int(bytes)
+        if bytes < 0:
+            raise InvalidField('BYTES: Number of bytes below zero.')
+        else:
+            return bytes
     except ValueError as e:
-        return f'ERROR: {e}'
-
-def fields_converter(fields: dict) -> dict:
-    ip = parse_ip(fields['ip'])
-    
+        raise InvalidField(f'BYTES: {e}')
 
 class LogEntry:
 
